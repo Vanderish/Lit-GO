84397914b626ad8ae93b0c3b2269fce7dc6d1b54
evan <mharisevan@gmail.com>
2026-08-25 13:28:02 +0700

    login page & protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,21 @@ import './App.css'; // Global import, tanpa module
 // Import komponen kamu
 import LandingPage from './pages/LandingPage/LandingPage';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Auth/Login';
+import ProtectedRoute from './routes/ProtectedRoutes';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route untuk halaman utama */}
+        {/* Route Publik */}
         <Route path="/" element={<LandingPage />} />
-        
-        {/* Route untuk dashboard */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Route yang Dilindungi (Hanya untuk user yang sudah login) */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </Router>
   );
